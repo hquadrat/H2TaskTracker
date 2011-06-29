2d2f83a2105d835c668bc0226b3d8cf55660dc7a
Henning T. Heil <henning@h-quadrat.com>
2011-06-29 02:27:05 +0200

    minor

--- application/views/statics/navigation.php
+++ application/views/statics/navigation.php
@@ -1,7 +1,7 @@
 <?php
 /* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * top panel navigation with help from
+ * http://net.tutsplus.com/tutorials/javascript-ajax/build-a-top-panel-with-jquery/
  */
 
 ?>
